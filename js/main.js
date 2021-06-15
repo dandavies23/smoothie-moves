7285e415c8ff0f98790606d7124f1dd764260f0f
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-06-15 19:28:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,71 +4,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /* When the page is loaded start game - consider inserting name and using to start game function*/
 
-    // setup game vars and cards
+        // setup game vars and cards
 
-    // fruit and veg pairs
+        // fruit and veg pairs
 
-    let fruitVegArray = [{
-            name: 'apple',
-            img: 'images/apple.png'
-        },
-        {
-            name: 'beetroot',
-            img: 'images/beetroot.png'
-        },
-        {
-            name: 'blueberries',
-            img: 'images/blueberries.png'
-        },
-        {
-            name: 'broccoli',
-            img: 'images/broccoli.png'
-        },
-        {
-            name: 'carrot',
-            img: 'images/carrot.png'
-        },
-        {
-            name: 'lemon',
-            img: 'images/lemon.png'
-        },
+        let fruitVegArray = [{
+                name: 'apple',
+                img: 'images/apple.png'
+            },
+            {
+                name: 'beetroot',
+                img: 'images/beetroot.png'
+            },
+            {
+                name: 'blueberries',
+                img: 'images/blueberries.png'
+            },
+            {
+                name: 'broccoli',
+                img: 'images/broccoli.png'
+            },
+            {
+                name: 'carrot',
+                img: 'images/carrot.png'
+            },
+            {
+                name: 'lemon',
+                img: 'images/lemon.png'
+            },
 
-    ]
+        ]
 
-    fruitVegArray = [...fruitVegArray, ...fruitVegArray]
-    // Randomise array using Math.random
-    fruitVegArray.sort(() => 0.5 - Math.random())
+        fruitVegArray = [...fruitVegArray, ...fruitVegArray]
+        // Randomise array using Math.random
+        fruitVegArray.sort(() => 0.5 - Math.random())
 
-    // Constants and empty arrays
-    const grid = document.querySelector('.grid-container') // picks up HTML
-    const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
-    let cardsWon = []
-    var barWidth = 0
+        // Constants and empty arrays
+        const grid = document.querySelector('.grid-container') // picks up HTML
+        const resultDisplay = document.querySelector('#result')
+        let cardsChosen = []
+        let cardsChosenId = []
+        let cardsWon = []
+        var barWidth = 0
 
-    // Set progress to 0
-    // document.getElementsByClassName('progress-bar').item(0).setAttribute('aria-valuenow', barWidth);
-    // document.getElementsByClassName('progress-bar').item(0).setAttribute('style','width:'+Number(barWidth)+'%');
+        // Set progress to 0
+        // document.getElementsByClassName('progress-bar').item(0).setAttribute('aria-valuenow', barWidth);
+        // document.getElementsByClassName('progress-bar').item(0).setAttribute('style','width:'+Number(barWidth)+'%');
 
-    //let scorebarelement = document.getElementById('score-bar')
-    //et scorebar.style.width = barWidth + '%'
+        //let scorebarelement = document.getElementById('score-bar')
+        //et scorebar.style.width = barWidth + '%'
 
-    // Create board
-    function createSmoothieGrid() {
-        for (let i = 0; i < fruitVegArray.length; i++) {
-            let card = document.createElement('img');
-            card.setAttribute('src', 'images/tumbler.png');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', tumblerLift)
-            grid.appendChild(card);
+        // Create board
+        function createSmoothieGrid() {
+            for (let i = 0; i < fruitVegArray.length; i++) {
+                let card = document.createElement('img');
+                card.setAttribute('src', 'images/tumbler.png');
+                card.setAttribute('data-id', i);
+                card.addEventListener('click', tumblerLift)
+                grid.appendChild(card);
+            }
         }
-    }
-
-    // Tumbler lift - if time want to work out lift animation 
-    function tumblerLift() {
+    
+  // Tumbler lift - if time want to work out lift animation 
+      function tumblerLift() {
+        // patch-2 begin
+        if(this.src==fruitVegArray[cardId].img){return null} //image already chosen
+        // patch-2 end
         let cardId = this.getAttribute('data-id')
-        // this.style.pointerEvents = 'none';
+        this.style.pointerEvents = 'none';
         cardsChosen.push(fruitVegArray[cardId].name)
         cardsChosenId.push(cardId)
         this.style.pointerEvents = 'auto';
@@ -76,7 +79,9 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(fruitVegArray[cardId])
         if (cardsChosen.length === 2) {
             setTimeout(checkForMatch, 500)
-            // this.style.pointerEvents = 'auto';
+            this.style.pointerEvents = 'auto';
+            cards[optionTwoId].style.pointerEvents = 'auto';
+            console.log(cardsWon)
         }
     }
 
@@ -99,8 +104,8 @@ document.addEventListener('DOMContentLoaded', () => {
         cardsChosenId = []
         resultDisplay.textContent = cardsWon.length
         barWidth = Math.round((cardsWon.length / 6) * 100) // converts cardsWon to percentage
-        document.getElementsByClassName('progress-bar').item(0).setAttribute('style', 'width:' + Number(barWidth) + '%');
-        document.getElementsByClassName('progress-bar').item(0).setAttribute('aria-valuenow', cardsWon.length);
+        document.getElementsByClassName('progress-bar').item(0).setAttribute('style', 'width:' + Number(barWidth) + '%'); 
+        document.getElementsByClassName('progress-bar').item(0).setAttribute('aria-valuenow', cardsWon.length); 
 
         if (cardsWon.length === fruitVegArray.length / 2) {
             resultDisplay.textContent = 'Smoothie is made! Press below to drink it up!'
